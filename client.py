4e8005676835b0352def54d59ce24b26db6a1c6a
vahagnd <vahagndovlatyan03@gmail.com>
2025-07-17 14:20:26 +0400

    almost working langchain agent with mcp

--- client.py
+++ client.py
@@ -1,21 +1,19 @@
 import asyncio
 from mcp.client.stdio import stdio_client
-from mcp import ClientSession
+from mcp import ClientSession, StdioServerParameters
 from langchain_mcp_adapters.tools import load_mcp_tools
-from langchain.agents import initialize_agent, Tool
+from langchain.agents import initialize_agent, Tool, AgentType, AgentExecutor
 from langchain_together import Together
 from dotenv import load_dotenv
+import json
 
 load_dotenv()  # Load TOGETHER_API_KEY
 
-llama = "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free"
-exaone = "lgai/exaone-3-5-32b-instruct"
-exaone_deep = "lgai/exaone-deep-32b"
-
-
 async def main():
-    # 1. Connect to your server
-    server_params = stdio_client(command="python", args=["server.py"])
+    server_params = StdioServerParameters(
+        command="python",
+        args=["server.py"]
+    )
 
     llama = "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free"
     exaone = "lgai/exaone-3-5-32b-instruct"
@@ -27,18 +25,34 @@ async def main():
         max_tokens=200
     )
 
-    async with server_params as (read, write):
+    async with stdio_client(server_params) as (read, write):
         async with ClientSession(read, write) as session:
             await session.initialize()
 
             tools = await load_mcp_tools(session)
-            agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+            agent = initialize_agent(
+                tools,
+                llm,
+                agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+                verbose=True
+            )
+
+            # Parsing error handling
+            agent_executor = AgentExecutor.from_agent_and_tools(
+                agent=agent.agent,
+                tools=tools,
+                verbose=False,
+                handle_parsing_errors=True  # Important
+            )
 
             tools_response = await session.list_tools()
             print("Available tools: ", [tool.name for tool in tools_response.tools])
 
             result = await session.read_resource("fedresurs://contracts/2025-01-20/2025-02-01")
-            contracts = result.content["contracts"]
+            contracts = json.loads(result.contents[0].text)["contracts"]
+
+            print("Contracts parsed:", len(contracts))
+            # print(f"Type: {type(contracts)}, Contracts: {contracts}")
 
             for i, contract in enumerate(contracts):
                 input_text = (
@@ -51,10 +65,9 @@ async def main():
 
                 print(f"\nОбработка контракта #{i + 1}/{len(contracts)}")
                 try:
-                    await agent.arun(input_text)
+                    await agent_executor.arun(input_text)
                 except Exception as e:
                     print(f"Ошибка при обработке: {e}")
 
-            print(result)
-
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
